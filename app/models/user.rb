79ddc079f6ae3a97c6f6d47dbdf02f2cbd96d365
Ivan Lucev <lucev.ivan@gmail.com>
2010-11-13 18:34:59 +0100

    added User model

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+# Schema version: 20101113162642
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  location   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email, :location
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, :presence   => true
+  
+  validates :email, :presence   => true
+                    :format     => { :with => email_regex }
+                    :uniqueness => { :case_sensitive => false }
+
+
+end
