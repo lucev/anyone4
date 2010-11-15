724360432e02aef457fa46af488da59c503e4b53
Ivan Lucev <lucev.ivan@gmail.com>
2010-11-16 00:34:46 +0100

    added user profile editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,30 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to Anyone 4!"
+      flash[:success] = "Welcome to Anyone4?"
       redirect_to @user
     else
       @title = "Sign up"
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+
 
 end
