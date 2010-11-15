44e50e4557cd9dc89ac115a69cd94456f4066337
Ivan Lucev <lucev.ivan@gmail.com>
2010-11-15 23:45:51 +0100

    added HTTP basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
   protect_from_forgery
   include SessionsHelper
+  
+  protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "blacknwhite" && password == "pusicamusica123"
+    end
+  end
 end
