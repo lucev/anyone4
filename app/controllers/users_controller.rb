a2b76bd3d1ed576e09913317e9abd9146b28f0d8
Ivan Lucev <lucev.ivan@gmail.com>
2010-11-16 02:13:21 +0100

    added user checking for editing profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def new
     @title = "Sign up"
     @user = User.new
@@ -37,6 +40,16 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
+  def authenticate
+    deny_access unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 
 
 end
