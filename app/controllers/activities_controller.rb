c85b56906004a1dc91dbb51d8b7c8e28b3677001
Ivan Lucev <lucev.ivan@gmail.com>
2010-11-18 02:05:23 +0100

    added posts on welcome page

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -11,7 +11,6 @@ class ActivitiesController < ApplicationController
     end
   end
 
-
   def destroy
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,16 @@ class PagesController < ApplicationController
   def home
     @title = "Home"
     @activity = Activity.new if signed_in?
+    @activities = Activity.all
   end
-  
+
   def about
     @title = "About"
   end
-  
+
   def contact
     @title = "Contact"
   end
 
 end
+
