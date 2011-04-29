eefc2fbf86fce1bd5aa8eed24bcdb0af7c3d4b00
Ivan Lucev <lucev.ivan@gmail.com>
2011-04-29 17:17:55 +0200

    HTTP auth not needed in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate
+  unless RAILS_ENV == 'development'
+    before_filter :authenticate
+  end
   protect_from_forgery
   include SessionsHelper
-  
+
   protected
   def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "blacknwhite" && password == "pusicamusica123"
+    unless RAILS_ENV == 'development'
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "blacknwhite" && password == "pusicamusica123"
+      end
     end
   end
 end
+
