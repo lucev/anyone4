af0baca0a616db50bf33935cdeaa6d58f4dfe920
Ivan Lucev <lucev.ivan@gmail.com>
2011-12-06 05:37:06 +0100

    fixed bug after removing HTTP authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  unless RAILS_ENV == 'development'
-    before_filter :authenticate
-  end
   protect_from_forgery
   include SessionsHelper
 end
