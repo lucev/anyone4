d9b9e36348c030be90ffc60893fb124d4fe989ec
Ivan Lucev <lucev.ivan@gmail.com>
2011-12-12 02:24:16 +0100

    email and password not required for fb user

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,16 +36,16 @@ class User < ActiveRecord::Base
 
   validates :name, :presence   => true
 
-  validates :email, :presence   => true,
-                    :format     => { :with => email_regex },
-                    :uniqueness => { :case_sensitive => false }
+#  validates :email, :presence   => true,
+#                    :format     => { :with => email_regex },
+#                    :uniqueness => { :case_sensitive => false }
 
-  validates :password, :presence     => true,
-                       :confirmation => true,
-                       :length       => { :within => 6..40 }
+#  validates :password, :presence     => true,
+#                       :confirmation => true,
+#                       :length       => { :within => 6..40 }
 
 
-  before_save :encrypt_password
+#  before_save :encrypt_password
 
   # Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
